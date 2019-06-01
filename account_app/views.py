39862787e6521428c1bdff5d1aa0e7c11193f7ce
BJTU16301106 <1009356468@qq.com>
2019-06-01 17:45:54 +0800

    login

--- account_app/views.py
+++ account_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.backends import ModelBackend
+from account_app.models import MyUser
+from django.contrib import auth
+from django.db.models import Q
+
+
+class MyBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = MyUser.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def registration(request):
+    return render(request, 'register.html')
