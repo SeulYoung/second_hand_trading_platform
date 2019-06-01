6522f196559ccf31b144c3c7266181fd9e0ffc29
SeulYoung <38755691+SeulYoung@users.noreply.github.com>
2019-06-01 21:50:20 +0800

    修改html

--- commodity_app/urls.py
+++ commodity_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.details, name="details"),
+    path('details/', views.details, name="details"),
 ]
--- commodity_app/views.py
+++ commodity_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Commodity
 
 
 def details(request):
-    return render(request, "details.html")
+    if request.method == "POST":
+        commodity_id = request.POST.get("commodity_id")
+        commodity = Commodity.objects.filter(commodity_id=commodity_id).first()
+        return render(request, "details.html", {"commodity": commodity})
