3ac399662fc9fc37eedc5a367659157fe7fa4d42
BJTU16301106 <1009356468@qq.com>
2019-06-03 20:26:22 +0800

    switch

--- account_app/urls.py
+++ account_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('login/', views.login, name="login"),
     path('registration/', views.registration, name="registration"),
+    path('switch/', views.switch, name="switch"),
+    path('logout/', views.logout, name="logout"),
 
 ]
