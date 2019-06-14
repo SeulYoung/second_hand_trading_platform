621123a74195fe58fee1564aa17e9bfd9fa0b61a
yyy <644751509@qq.com>
2019-06-14 14:59:08 +0800

    联系卖家

--- static/js/hi/chatSeller.js
+++ static/js/hi/chatSeller.js
@@ -1,3 +1,20 @@
-function chatWithSeller() {
-    
+function chatWithSeller(seller_name) {
+    let form = document.createElement("form");
+    form.action = "/chatRoom/";
+    form.method = "POST";
+
+    let data = {};
+
+    for (let key in data) {
+        let input = document.createElement("input");
+        input.type = "hidden";
+        input.name = key;
+        input.value = data[key];
+
+        form.appendChild(input);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
 }
